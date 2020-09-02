7da34dc41319c7da82fce4e1d5e11ee0863744a7
karthikb4ps@gmail.com <karthikb4ps@gmail.com>
2020-09-01 23:34:10 -0400

    Initial Commit

---
+++ oauth-client-app/src/test/java/com/demo/oauthclientapp/OauthClientAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.oauthclientapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthClientAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
